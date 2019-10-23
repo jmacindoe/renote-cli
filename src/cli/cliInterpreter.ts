6be77ab87d9fbae112ffa4b83e27aadb61bba296
James Macindoe <james.macindoe@gmail.com>
2019-10-23 14:37:12 +1100

    undefined instead of null

--- src/cli/cliInterpreter.ts
+++ src/cli/cliInterpreter.ts
@@ -5,21 +5,21 @@ import { ExhaustiveSwitchError } from "../error/ExhaustiveSwitchError"
 
 export async function cliInterpreter(cli: () => CliComponent) {
   const component = cli()
-  var next: any | null = null
+  var next: any | undefined = undefined
   var request: IteratorResult<CliOperation>
   do {
-    request = await component.next(...(next === null ? [] : [next]))
+    request = await component.next(next)
     if (request.value) {
       next = await execute(request.value)
     }
   } while (!request.done)
 }
 
-async function execute(request: CliOperation): Promise<any | null> {
+async function execute(request: CliOperation): Promise<any | undefined> {
   switch (request.type) {
     case "print":
       console.log(request.text)
-      break
+      return undefined
     case "prompt":
       return await inquirer.prompt(request.questions)
     default:
